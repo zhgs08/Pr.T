8b9ea7c88f663e669abbb903661d0d0b097f3bf5
zhgs08 <gaukharzharkeyeva@gmail.com>
2016-01-26 00:15:38 +0600

    no read key

--- helloworld.cs
+++ helloworld.cs
@@ -11,7 +11,6 @@ namespace ConsoleApplication3
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
-            Console.ReadKey();
         }
     }
 }
