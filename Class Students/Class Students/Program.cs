2d2ca11cf793d2d776dc1cebcefd0eb4edb49bca
zhgs08 <gaukharzharkeyeva@gmail.com>
2016-02-12 01:23:03 +0600

    deeee

---
+++ Class Students/Class Students/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Students
+{
+        class Student
+        {
+            public string firstName;
+            public string lastName;
+            public int age;
+            public string id;
+            public Student(string _firstName, string _lastName, int _age, string _id)
+            {
+                firstName = _firstName;
+                lastName = _lastName;
+                age = _age;
+                id = _id;
+            }
+            public override string ToString()
+            {
+                return firstName + " " + lastName + " " + "age - " + age + " ," + "id -" + id;
+            }
+            static void Main(string[] args)
+            {
+                Student s = new Student("Gaukhar", "Zharkeyeva", 18, "15BD02034");
+                Console.WriteLine(s);
+                Console.ReadKey();
+            }
+        }
+    
+}
---
+++ Lab1/prime.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] v = Console.ReadLine().Split(); // считывание массива строк 
+            foreach(string s in v)                  // пробег по всем строкам v 
+            {
+                int cnt=0;
+                int jk = Convert.ToInt32(s);       // конвертирование строки в целое число 
+                for(int i=1; i<=jk; i++)          // проверка на простоту числа 
+                {
+                    if (jk % i == 0)
+                        cnt++;
+                }
+                if (cnt == 2)
+                    Console.WriteLine(jk);
+            }
+            Console.ReadKey();
+        }
+    }
+}
