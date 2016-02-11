727e1d262b1b20c2e178d493dd6f725418d77cf3
zhgs08 <gaukharzharkeyeva@gmail.com>
2016-02-12 01:58:25 +0600

    lab2-3

---
+++ Lab2-3/Lab2-3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_3
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            FileStream finput = new FileStream("input.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            FileStream foutput = new FileStream("output.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamReader sr = new StreamReader(finput);
+            StreamWriter sw = new StreamWriter(foutput);
+            string s = sr.ReadLine();
+            string[] token = s.Split();
+            int min = int.Parse(token[0]);
+            foreach (string b in token)
+            {
+                int a = int.Parse(b);
+                bool p = false;
+                for (int i = 2; i * i <= a; i++)
+                {
+                    if (a % i == 0)
+                        p = true;
+                }
+                if (p == false)
+                {
+                    if (a <= min)
+                        min = a;
+                }
+            }
+            sw.WriteLine(min);
+            sw.Close();
+            sr.Close();
+            finput.Close();
+            foutput.Close();
+
+            Console.ReadKey();
+
+        }
+    }
+}
