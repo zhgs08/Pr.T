42737fc00d31318f2eaef7cc2b794d2bbeb7aafd
zhgs08 <gaukharzharkeyeva@gmail.com>
2016-02-12 02:00:05 +0600

    lab2-additional

---
+++ Lab2-additional/Lab2-additional/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_additional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo v = new DirectoryInfo(@"\\psf\Home\Desktop\Gaukhar");
+
+            int index = 0;
+            FileInfo[] files = v.GetFiles();
+            bool showFileData = false;
+            while (true)
+            {
+                if (showFileData == false)
+                {
+                    for (int i = 0; i < files.Length; i++)
+                    {
+                        if (index == i)
+                        {
+                            Console.BackgroundColor = ConsoleColor.White;
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                        }
+                        else
+                        {
+                            Console.BackgroundColor = ConsoleColor.Black;
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        Console.WriteLine(files[i].Name);
+                    }
+                }
+                ConsoleKeyInfo button = Console.ReadKey();
+                if (button.Key == ConsoleKey.UpArrow)
+                {
+                    if (index > 0)
+                        index--;
+                }
+                if (button.Key == ConsoleKey.DownArrow)
+                {
+                    if (index < files.Length - 1)
+                        index++;
+                }
+                if (button.Key == ConsoleKey.Enter)
+                {
+                    FileStream f = new FileStream(files[index].FullName, FileMode.OpenOrCreate, FileAccess.Read);
+                    StreamReader sr = new StreamReader(f);
+                    Console.Write(sr.ReadToEnd());
+                    sr.Close();
+                    f.Close();
+                    showFileData = true;
+
+                }
+                if (button.Key == ConsoleKey.Escape)
+                    showFileData = false;
+
+                if (showFileData == false)
+                    Console.Clear();
+            }
+            Console.ReadKey();
+        }
+    }
+}
